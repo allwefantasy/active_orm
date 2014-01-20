987cdaf4f9b45d460e3f31b5069088dbd4fbd0a1
WilliamZhu <allwefantasy@gmail.com>
2014-01-20 17:02:06 +0800

    修正多对一关系时的一个问题

--- src/main/java/net/csdn/jpa/association/Association.java
+++ src/main/java/net/csdn/jpa/association/Association.java
@@ -106,9 +106,10 @@ public class Association {
 
     private JPQL jpql() {
         JPQL jpql = null;
-        if (type.equals("javax.persistence.ManyToMany") || type.equals("javax.persistence.OneToMany")) {
+        if (type.equals("javax.persistence.ManyToMany") || type.equals("javax.persistence.ManyToOne")) {
             jpql = (JPQL) ReflectHelper.staticMethod(getTargetModelClass(), "joins", targetField + " as framework_service_holder_" + targetField);
         }
+
         if (jpql != null) {
             jpql.where(map("framework_service_holder_" + targetField, object));
         } else {
